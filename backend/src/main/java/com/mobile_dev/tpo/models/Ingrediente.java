514ecde1d490cc5c3b64d770f74770a0fc978602
Julian-Bonavota <julianbonavota@gmail.com>
2025-03-30 17:20:27 -0300

    cositas

--- backend/src/main/java/com/mobile_dev/tpo/models/Ingrediente.java
+++ backend/src/main/java/com/mobile_dev/tpo/models/Ingrediente.java
@@ -1,9 +1,17 @@
 package com.mobile_dev.tpo.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ingredientes")
 public class Ingrediente {
-    private int idingrediente;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idingrediente")
+    private Long idIngrediente;
+
+    @Column(name = "nombre")
     private String nombre;
-
-
 }
 
